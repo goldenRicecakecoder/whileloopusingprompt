7b75a99690097297184f87caee65e35f2fda9264
goldenRicecakecoder <89317269+goldenRicecakecoder@users.noreply.github.com>
2024-03-11 20:44:58 -0500

    Add files via upload

---
+++ useprompt.js
@@ -0,0 +1,10 @@
+// Display 1 to 50 numbers using loop and prompt
+
+let a = Number(prompt("Enter the number limit to display"));
+// console.log(a);
+// console.log(typeof(a));
+
+while(a>=0){
+    console.log(a);
+    a--;
+}
